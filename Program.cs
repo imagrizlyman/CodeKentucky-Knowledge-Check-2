873684f8a5ddabe353c405ff5fd6a73676fb5923
Austin Hacker <austin.hacker@yahoo.com>
2022-11-07 18:44:18 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Knowledge_Check_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
